65944c88249e20cc5a2d78d93d77b6e8c100751c
Tyler Furby <tyler@tylerfurby.com>
2017-08-24 04:14:18 -0600

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-window.onresize = displayWindowSize;
-window.onload = displayWindowSize;
-function displayWindowSize() {
-  myWidth = window.innerWidth;
-  myHeight = window.innerHeight;
-    // your size calculation code here
-    document.getElementById("dimensions").innerHTML = "Development: " + myWidth + "x" + myHeight;
-};
